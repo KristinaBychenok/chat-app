3a3a14c78e601abcd5cba58ffde11bd5539cae37
Krystsina Bychanok <Krystsina_Bychanok@epam.com>
2023-10-18 13:12:42 +0200

    Add main pages to authentication

---
+++ components/layout/layout.tsx
@@ -0,0 +1,12 @@
+import MainNavigation from "./main-navigation";
+
+function Layout(props: any) {
+  return (
+    <>
+      <MainNavigation />
+      <main>{props.children}</main>
+    </>
+  );
+}
+
+export default Layout;
